d44ca39dafa7e1b971bf0b6a91485c58fdfdc0a1
Mridula Shanker <Mridula Shanker@LAPTOP-T2PHJ0FM>
2019-02-06 22:22:55 -0500

    2/6/19

--- Salary Calculator/src/SalaryCalculator.java
+++ Salary Calculator/src/SalaryCalculator.java
@@ -18,17 +18,21 @@ public class SalaryCalculator {
 		hourString.setBounds(40,30,250,30);
 		myFrame.add(hourString);
 		
-		JTextField hourIn = new JTextField();
-		hourIn.setBounds(125,30,250,30);
-		myFrame.add(hourIn);
+		JTextField hRate = new JTextField();
+		hRate.setBounds(125,30,250,30);
+		myFrame.add(hRate);
 		
 		JLabel weekString = new JLabel("Hours/Week: ");
 		weekString.setBounds(40,90,250,30);
 		myFrame.add(weekString);
 		
-		JTextField weekIn = new JTextField();
-		weekIn.setBounds(125,90,250,30);
-		myFrame.add(weekIn);
+		JTextField hourIn = new JTextField();
+		hourIn.setBounds(125,90,250,30);
+		myFrame.add(hourIn);
+	
+		final JLabel dispText = new JLabel();
+		dispText.setBounds(275,160,500,30);
+		myFrame.add(dispText);
 		
 		//JCheckBox try = new JCheckBox("Try This");
 		//try.setBounds(40,140,15,15);
@@ -36,14 +40,29 @@ public class SalaryCalculator {
 		
 		JButton command = new JButton("Calculate");
 		command.setBounds(40,165,200,30);
+		/**
 		command.addActionListener(new ActionListener()
 				{
 					public void actionPerformed(ActionEvent e)
 					{
-						
-						
+				        if(try.isSelected()) 
+				        {//checkbox has been selected
+				        	int totalHours = Integer.parseInt(hourIn.getText());
+				        	int rate = 40;
+				        	int annualSalary = totalHours * rate;
+				        	dispName.setText("Annual Salary", annualSalary);
+				        			
+				        } 
+				        else 
+				        {//checkbox has been deselected
+				        	int totalHours = Integer.parseInt(hourIn.getText());
+				        	int rate = Integer.parseInt(hRate.getText());
+				        	int annualSalary = totalHours * rate;	
+				        	dispName.setText("Annual Salary", annualSalary);
+				        };				
 					}
 				});
+				**/
 		myFrame.add(command);
 		myFrame.setVisible(true);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
